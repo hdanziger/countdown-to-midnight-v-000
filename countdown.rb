6f557633068c0bf5759b79e0b5ef94f0c12f83b4
Hannah Danziger <hdanziger8@gmail.com>
2017-09-20 14:24:07 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,18 @@
 #write your code here
 
-def countdown (n)
-  while n > 0
-    puts "#{n} SECOND(S)!"
-    n -= 1
+def countdown(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    seconds_to_midnight -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep (n)
-  while n > 0
-    puts "#{n} SECOND(S)!"
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
     sleep(1)
-    n -= 1
+    seconds_to_midnight -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
