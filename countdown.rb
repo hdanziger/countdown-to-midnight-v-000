648ad176b57ee8ad882c6304b8ffcf9032870849
Hannah Danziger <hdanziger8@gmail.com>
2017-09-20 14:22:40 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown (n)
   end
   puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep (n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    sleep(1)
+    n -= 1
+  end
+  puts "HAPPY NEW YEAR!"
+end
